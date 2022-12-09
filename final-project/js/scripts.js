360accfe3b508aea10d90bdf4b01909ab867e0ba
Anita Cheng <anitacheng19@gmail.com>
2022-12-09 17:52:10 -0500

    fixed academics

--- final-project/js/scripts.js
+++ final-project/js/scripts.js
@@ -1,20 +1,22 @@
 const sections = document.querySelectorAll(".content-description")
 const name = document.getElementById("name");
 const subname = document.getElementById("subname");
-const description = document.getElementById("description");
+const description = document.getElementById("description").querySelector('li');
+const image = document.getElementById('image').querySelector('img');
+
 const callback = (entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            console.log(subname);
             name.innerText = entry.target.dataset.name;
             subname.innerText = entry.target.dataset.subname;
             description.innerText = entry.target.dataset.description;
+            image.src = entry.target.dataset.image;
+            image.alt = entry.target.dataset.alt;
         }
     });
 };
-
 const options = {
-    threshold: 1,
+    threshold: 0.1,
 };
 
 let observer = new IntersectionObserver(callback, options);
