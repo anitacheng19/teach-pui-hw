d9ca4661f3c6f86d2a66082df8767e5fbe8cc358
Anita Cheng <anitacheng19@gmail.com>
2022-12-10 23:26:56 -0500

    including API

--- final-project/js/scripts.js
+++ final-project/js/scripts.js
@@ -1,24 +1,24 @@
-const sections = document.querySelectorAll(".content-description")
-const name = document.getElementById("name");
-const subname = document.getElementById("subname");
-const description = document.getElementById("description").querySelector('li');
-const image = document.getElementById('image').querySelector('img');
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-const callback = (entries) => {
+  const scrollList = document.querySelectorAll(".project")
+
+  const callback = (entries, observer) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            name.innerText = entry.target.dataset.name;
-            subname.innerText = entry.target.dataset.subname;
-            description.innerText = entry.target.dataset.description;
-            image.src = entry.target.dataset.image;
-            image.alt = entry.target.dataset.alt;
-        }
-    });
-};
-const options = {
-    threshold: 0.1,
-};
 
-let observer = new IntersectionObserver(callback, options);
+      if (entry.isIntersecting) {
 
-sections.forEach((section) => observer.observe(section));
+        entry.target.classList.add("visible");
+
+      }
+      })
+  }
+  
+  const options = {}
+  
+  const myObserver = new IntersectionObserver(callback, options)
+
+  scrollList.forEach(scrollItem => {
+    myObserver.observe(scrollItem)
+  })
+
+});
